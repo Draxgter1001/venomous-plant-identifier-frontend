50bce6e196d1c5b49128d80f40524dafe9e2cd23
Tafshi Uthshow Hoque <69767127+Draxgter1001@users.noreply.github.com>
2025-04-24 18:33:24 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ function Footer() {
     return(
         <footer className='footer'>
             <p>&copy; {new Date().getFullYear()} Venomous Plant Identifier. All rights reserved.</p>
+            <p>Please do not rely on our tool entirely as it is not 100% accurate</p>
             <p>
                 Powered by <a href="https://www.kindwise.com/plant-id" target="_blank" rel="noopener noreferrer">Plant.id API</a>
             </p>
